9ac2a006b583076e721bd9f662fedfbdb84f4125
rockyxd3 <shivamlkr629@gmail.com>
2026-05-21 20:53:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,6 +24,6 @@ except:
     LOG_CHANNEL_ID = 0
 
 # ----------------- AI & SYSTEM KEYS ----------------- #
-GIT_TOKEN = os.environ.get("GIT_TOKEN", "")
+GIT_TOKEN = os.environ.get("GIT_TOKEN", "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")
 HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", "")
